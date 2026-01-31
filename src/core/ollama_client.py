a0186f5006b7e62dd0ecf64075e820a7f1accd91
Akshay Ravirala <92001850+Akshay0649@users.noreply.github.com>
2026-01-31 07:58:45 +0000

    Make Ollama client Docker-aware

--- src/core/ollama_client.py
+++ src/core/ollama_client.py
@@ -1,12 +1,15 @@
 import ollama
+import os
 from .base_llm import BaseLLM
 
 class OllamaLLM(BaseLLM):
     def __init__(self, model="mistral"):
         self.model = model
+        host = os.getenv("OLLAMA_HOST", "http://localhost:11434")
+        self.client = ollama.Client(host=host)
 
     def generate(self, prompt):
-        response = ollama.chat(
+        response = self.client.chat(
             model=self.model,
             messages=[{"role": "user", "content": prompt}]
         )
